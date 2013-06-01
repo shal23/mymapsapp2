c506487d116dafee8173af8f84688d896475e046
Shalini <shalini.dalal@yahoo.com.au>
2013-06-01 14:42:15 +1000

    rails_apps_composer: initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Mymapsapp2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymapsapp2::Application.config.secret_token = 'c11f8600fc94ec84f574dd41da548d7341f67e91ec742a2882948943476d011fb04fbfdaa1d288ff9664201b8b85b09e435783c19246cff6c6c9d12868bbac62'
